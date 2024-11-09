64bdba1c2c70d69b1c483892e1db2c1fd1402f05
AlexPerin1983 <windowfilm.br@gmail.com>
2024-11-09 10:23:38 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,20 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+import react from "@vitejs/plugin-react-swc";
+import { componentTagger } from "invoke-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  
-
+  base: '/tintpro/',
   server: {
     host: "::",
-    port: 8080,
+    port: 3000,
     base: '/tintpro/',
   },
   plugins: [
     react(),
-    mode === 'development' && componentTagger(),
-  ].filter(Boolean),
+    componentTagger(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
